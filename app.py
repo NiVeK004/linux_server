242ea99783de43ea4e6158b727c9fc18c3b4ccc7
NiVeK004 <jacobsenkevin04@gmail.com>
2025-03-27 04:04:47 +0100

    Fikset database oppstart og fjernet unødvendig kode

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-GNU nano 7.2
 from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import psycopg2
@@ -6,7 +5,10 @@ import psutil # For systemstatistikk
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://monitor_admin:nftn47@>db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://monitor_admin:nftn47@localhost/server_monitor'
+
+
+db = SQLAlchemy(app)
 
 class Metrics(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,17 +20,8 @@ class Metrics(db.Model):
 
 @app.route('/')
 def index():
-    metrics = Metrics.query.order_by(Metrics.timestamp.desc()).limit(10).al>    return render_template('index.html', metrics=metrics)
-
-# Ny API rute for systemstatistikk i "real-time"!
-@app.route('/system-stats')
-def system_stats():
-        stats = {
-            "cpu_usage": psutil.cpu_percent(interval=1),
-            "ram_usage": psutil.virtual_memory().percent,
-            "disk_usage": psutil.disk_usage('/').percent,
-        }
-        return stats
+    metrics = Metrics.query.order_by(Metrics.timestamp.desc()).limit(10).all() 
+    return render_template('index.html', metrics=metrics)
 
 # Ny API rute for systemstatistikk i "real-time"!
 @app.route('/system-stats')
