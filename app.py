a9d378092522376da55088914042792366502506
NiVeK004 <jacobsenkevin04@gmail.com>
2025-03-27 02:25:31 +0100

    app.py med live data rute (API)

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+  GNU nano 7.2                       app.py                                 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import psycopg2
+import psutil # For systemstatistikk
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://monitor_admin:nftn47@localhost/server_monitor'
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://monitor_admin:nftn47@>db = SQLAlchemy(app)
 
 class Metrics(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,8 +17,17 @@ class Metrics(db.Model):
 
 @app.route('/')
 def index():
-    metrics = Metrics.query.order_by(Metrics.timestamp.desc()).limit(10).all()  # Siste 10 "records"
-    return render_template('index.html', metrics=metrics)
+    metrics = Metrics.query.order_by(Metrics.timestamp.desc()).limit(10).al>    return render_template('index.html', metrics=metrics)
+
+# Ny API rute for systemstatistikk i "real-time"!
+@app.route('/system-stats')
+def system_stats():
+        stats = {
+            "cpu_usage": psutil.cpu_percent(interval=1),
+            "ram_usage": psutil.virtual_memory().percent,
+            "disk_usage": psutil.disk_usage('/').percent,
+        }
+        return stats
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
